c6300fcf95904143c69489a97f10563606a0e556
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-30 15:47:57 +0700

    Create page: RoomDetails

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<head>
-				<title>...</title>
+				<link
+					rel='stylesheet'
+					href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
+					crossOrigin='anonymous'
+				/>
 			</head>
 			<body className={poppins.className}>
 				<NextAuthProvider>
